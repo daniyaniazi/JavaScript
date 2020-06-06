6b84bef0cc736833478c9bde68a8533e72cf064a
daniyaniazi <farihadania@hotmail.com>
2020-06-06 17:04:51 +0500

    add variable files
    chap 2 assignment

---
+++ Saylani_Assignment/ASS_5/chap_2_variables/app.js
@@ -0,0 +1,34 @@
+document.write("<h1> VARIABLES </h1>");
+
+var username = "daniyaniazi";
+var myName = "Daniya";
+var fullName = myName + "Niazi";
+
+
+var message = "Hello World";
+alert(message);
+
+// var name = prompt("enter name");
+// var age = prompt("enter age");
+// var degree = prompt("enter degree");
+// alert(name);
+// alert(age);
+// alert(degree);
+
+
+var pizza = "PIZZA";
+alert(pizza + "\n" + pizza.slice(0, 4) + "\n" + pizza.slice(0, 3) + "\n" + pizza.slice(0, 2) + "\n" + pizza.slice(0, 1));
+
+
+
+var email = "farihadania@hotmail.com";
+alert("My email address is " + email);
+
+var book = "Asmarter way to learn JavaScript";
+alert("I am trying to learn from the book  " + email);
+
+
+document.write("<h2>Yah! I can write HTML content through javascript </h2>");
+
+var pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(pattern)
