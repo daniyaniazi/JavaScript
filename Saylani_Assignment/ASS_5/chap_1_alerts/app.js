8bedca92e340331eb1cce6ef2ca645b1800b9887
daniyaniazi <farihadania@hotmail.com>
2020-06-06 16:45:25 +0500

    added JS_Assignment file
    make use of alerts

---
+++ Saylani_Assignment/ASS_5/chap_1_alerts/app.js
@@ -0,0 +1,8 @@
+document.write("<h1> Alerts </h1>")
+
+alert("welcome to my website")
+alert("ERROR !1 please enter a valid password")
+alert("welcome to JS land.. \nHappy Coding!")
+
+alert("welcome to JS Land..")
+alert("Happy Coding!")
