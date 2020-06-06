148f14f01fa4cb2b7a1e4c8fd7c19c7f0521cd46
daniyaniazi <farihadania@hotmail.com>
2020-06-06 17:18:57 +0500

    added variable_numbers file
    chap_3 Assignment

---
+++ Saylani_Assignment/ASS_5/chap_3_variables_numbers/app.js
@@ -0,0 +1,19 @@
+document.write("<h1> VARIABLES FOR NUMBERS </h1>");
+
+
+
+
+var age = "19";
+alert("i am " + age + " years old");
+
+var noOfVisit = 10;
+alert("you have visited " + noOfVisit + "  times");
+
+var birthYear = 2000;
+document.write("My birth year is " + birthYear)
+document.write(" <br> DataType of my declared variable is " + typeof birthYear)
+
+var visitorName = "jhon";
+var profuctTitle = "T-Shirst";
+var quantity = 5;
+document.write(" <br>" + visitorName + " ordered " + quantity + "  " + profuctTitle + " on XYZ Clothing store");
