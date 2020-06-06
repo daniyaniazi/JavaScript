a83521e4f5d1085e7d17394800f9a4aea0ce4bd2
daniyaniazi <farihadania@hotmail.com>
2020-06-06 17:57:02 +0500

    added legal and illegal files
    chap_4 assignment

---
+++ Saylani_Assignment/ASS_5/chap_4_legel_illegal_var/app.js
@@ -0,0 +1,25 @@
+document.write("<h1> VARIABLES  legels & illegal </h1>");
+
+
+var variable1 = "Hello World!",
+    variable2 = "javascript",
+    variable3 = "saylani";
+
+var var1 = "var1",
+    $var1 = "$var1",
+    var_1 = "var_1",
+    _var = "_var",
+    _1var = "_1var";
+document.write("<h2> Rules for naming JS variable</h2>")
+document.write("<br>")
+
+document.write("Variable names can only contain numbers, letters,$ and _.For example $my_1stVariable")
+document.write("<br>")
+
+document.write("Variables must begin with a $,letter or _. For example $name, _name or name")
+document.write("<br>")
+
+document.write(" Variable names are case Sensitive")
+document.write("<br>")
+
+document.write(" Variable names should not be JS keyword")
