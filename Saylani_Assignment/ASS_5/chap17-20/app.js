046e7690b010ec95258d94f3e737c6a1bfb1e3a7
daniyaniazi <farihadania@hotmail.com>
2020-06-08 23:46:16 +0500

    added some more task

---
+++ Saylani_Assignment/ASS_5/chap17-20/app.js
@@ -0,0 +1,83 @@
+// 1
+let multiArr = [];
+// 2
+multiArr = [
+    [0, 1, 2, 3],
+    [1, 0, 1, 2],
+    [2, 1, 0, 1]
+];
+// 3
+let counting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+counting.forEach(num => {
+    document.write(`${num} <br>`);
+
+});
+// //4
+// let tableNo = +prompt('Enter number to show its multiplication table');
+// let length = +prompt('Enter length multiplication table');
+// for (let i = 1; i <= length; i++) {
+//     document.write(`${tableNo}x${i}=${tableNo*i}<br>`);
+// }
+
+// 5
+fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry']
+for (let j = 0; j < fruits.length; j++) {
+    document.write(`Element at index ${j} is ${fruits[j]}<br>`);
+}
+
+// 6
+document.write(`<br>`);
+// a
+document.write("Counting:");
+for (let k = 0; k <= 15; k++) {
+    document.write(`${k} ,`);
+
+}
+document.write(`<br>`);
+// b
+document.write("Reverse counting:");
+for (let l = 10; l > 0; l--) {
+    document.write(`${l} ,`);
+
+}
+document.write(`<br>`);
+// c
+document.write("Even :");
+for (let m = 0; m <= 20; m += 2) {
+    document.write(`${m} ,`);
+
+}
+document.write(`<br>`);
+// d
+document.write("Odd :");
+for (let n = 1; n <= 20; n += 2) {
+    document.write(`${n} ,`);
+
+}
+document.write(`<br>`);
+// d
+document.write("Series:");
+for (let o = 2; o <= 20; o += 2) {
+    document.write(`${o}k ,`);
+
+}
+document.write(`<br>`);
+// 7
+let input = prompt("Welcome to ABC bakery! What do you want to order Sir/Ma'am? ");
+let A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+
+// 8
+
+
+
+// 9
+
+
+
+// 10
+document.write(`<br>`);
+
+for (let p = 5; p <= 100; p += 5) {
+    document.write(`${p} ,`);
+}
